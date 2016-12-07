e85f6d1d8232a16796134e9f2df80ffd3fa21be7
KonradWinkowski <winkowskikonrad@gmail.com>
2016-12-06 22:30:03 -0500

    Removed Hairline from navigationbars
    added call to remove the hairline from navigation bars for a more
    “Flat” look

--- sample_project/Utility/CustomNavigationController/CustomNavigationViewController.m
+++ sample_project/Utility/CustomNavigationController/CustomNavigationViewController.m
@@ -16,11 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView *hairlineImageView = [self findHairlineImageViewUnder:self.navigationBar];
+    
+    if (hairlineImageView) {
+        hairlineImageView.hidden = YES;
+    }
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIImageView *)findHairlineImageViewUnder:(UIView *)view {
+    
+    if ([view isKindOfClass:UIImageView.class] && view.bounds.size.height <= 1.0) {
+        return (UIImageView *)view;
+    }
+    
+    for (UIView *subview in view.subviews) {
+        UIImageView *imageView = [self findHairlineImageViewUnder:subview];
+        if (imageView) {
+            return imageView;
+        }
+    }
+    
+    return nil;
 }
 
 /*
